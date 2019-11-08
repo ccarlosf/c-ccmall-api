88a64f8db1e97a942ac2d5d84a31309895dcbd0e
ccarlos <2238513100@qq.com>
2019-11-08 23:15:31 +0800

    获取分类六宫格数据 Api测试数据

---
+++ src/main/java/com/ccarlos/cccmall/controller/CategoryController.java
@@ -0,0 +1,37 @@
+package com.ccarlos.cccmall.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.ccarlos.cccmall.data.CategoryTestData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * @description: 分类控制器
+ * @author: Created by ccarlos
+ * @date: 2019/11/8 23:07
+ */
+@Slf4j
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    /**
+     * @description: 获取分类六宫格数据
+     * @author: ccarlos
+     * @date: 2019/11/8 23:11
+      * @param: request
+     * @return: java.lang.String
+     */
+    @GetMapping("/grid/all")
+    public String getCategoryByGrid(HttpServletRequest request){
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        log.info("获取分类六宫格数据,请求参数parameterMap:{}", JSON.toJSONString(parameterMap));
+        log.info("获取分类六宫格数据,,请求返回数据:{}", CategoryTestData.getCategoryByGrid);
+        return CategoryTestData.getCategoryByGrid;
+    }
+}
---
+++ src/main/java/com/ccarlos/cccmall/data/CategoryTestData.java
@@ -0,0 +1,60 @@
+package com.ccarlos.cccmall.data;
+
+/**
+ * @description: 分类Api测试数据
+ * @author: Created by ccarlos
+ * @date: 2019/11/8 23:09
+ */
+public class CategoryTestData {
+
+    public static String getCategoryByGrid="[\n" +
+            "    {\n" +
+            "        \"id\": 1,\n" +
+            "        \"title\": \"服装\",\n" +
+            "        \"img\": \"http://i1.sleeve.7yue.pro/grid/clothing.png\",\n" +
+            "        \"name\": null,\n" +
+            "        \"category_id\": null,\n" +
+            "        \"root_category_id\": 2\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"id\": 2,\n" +
+            "        \"title\": \"包包\",\n" +
+            "        \"img\": \"http://i1.sleeve.7yue.pro/grid/bag.png\",\n" +
+            "        \"name\": null,\n" +
+            "        \"category_id\": null,\n" +
+            "        \"root_category_id\": 3\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"id\": 3,\n" +
+            "        \"title\": \"鞋\",\n" +
+            "        \"img\": \"http://i1.sleeve.7yue.pro/grid/shoes.png\",\n" +
+            "        \"name\": null,\n" +
+            "        \"category_id\": null,\n" +
+            "        \"root_category_id\": 1\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"id\": 4,\n" +
+            "        \"title\": \"饰品\",\n" +
+            "        \"img\": \"http://i1.sleeve.7yue.pro/grid/jewelry.png\",\n" +
+            "        \"name\": null,\n" +
+            "        \"category_id\": null,\n" +
+            "        \"root_category_id\": 5\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"id\": 5,\n" +
+            "        \"title\": \"居家\",\n" +
+            "        \"img\": \"http://i1.sleeve.7yue.pro/grid/furniture.png\",\n" +
+            "        \"name\": null,\n" +
+            "        \"category_id\": null,\n" +
+            "        \"root_category_id\": 4\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"id\": 6,\n" +
+            "        \"title\": \"工艺\",\n" +
+            "        \"img\": \"http://i1.sleeve.7yue.pro/grid/book.png\",\n" +
+            "        \"name\": null,\n" +
+            "        \"category_id\": null,\n" +
+            "        \"root_category_id\": 24\n" +
+            "    }\n" +
+            "]";
+}
