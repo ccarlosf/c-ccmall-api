a86d5b6b901a18ddcf665719d542462092532611
ccarlos <2238513100@qq.com>
2019-11-09 16:52:54 +0800

    主题测试数据修改

--- src/main/java/com/ccarlos/cccmall/data/ThemeTestData.java
+++ src/main/java/com/ccarlos/cccmall/data/ThemeTestData.java
@@ -9,7 +9,7 @@ public class ThemeTestData {
     /**
      * 单个主题报文数据
      */
-    public static String singleThemeStr = "[\n" +
+    public static String singleThemeStr ="[\n" +
             "    {\n" +
             "        \"id\": 1,\n" +
             "        \"title\": \"清凉一夏，折扣季\",\n" +
@@ -21,10 +21,46 @@ public class ThemeTestData {
             "        \"title_img\": \"\",\n" +
             "        \"tpl_name\": \"janna\",\n" +
             "        \"online\": true\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"id\": 4,\n" +
+            "        \"title\": \"每周上新\",\n" +
+            "        \"description\": \"风袖`每周上新`活动#每周都有一款折扣商品，每周都有适合你的唯一专属#有Ins复古风装饰；金属CD唱片夹；每周来逛逛，找到你所喜爱的东西\",\n" +
+            "        \"name\": \"t-2\",\n" +
+            "        \"entrance_img\": null,\n" +
+            "        \"extend\": null,\n" +
+            "        \"internal_top_img\": \"http://i2.sleeve.7yue.pro/m1.png\",\n" +
+            "        \"title_img\": \"http://i2.sleeve.7yue.pro/m3.png\",\n" +
+            "        \"tpl_name\": null,\n" +
+            "        \"online\": true\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"id\": 5,\n" +
+            "        \"title\": \"风袖甄选\",\n" +
+            "        \"description\": \"甄选\",\n" +
+            "        \"name\": \"t-3\",\n" +
+            "        \"entrance_img\": \"http://i2.sleeve.7yue.pro/m9.png\",\n" +
+            "        \"extend\": null,\n" +
+            "        \"internal_top_img\": \"http://i2.sleeve.7yue.pro/m11.png\",\n" +
+            "        \"title_img\": \"\",\n" +
+            "        \"tpl_name\": \"diana\",\n" +
+            "        \"online\": true\n" +
+            "    },\n" +
+            "    {\n" +
+            "        \"id\": 6,\n" +
+            "        \"title\": \"时尚穿搭\",\n" +
+            "        \"description\": \"帅点才有女朋友\",\n" +
+            "        \"name\": \"t-4\",\n" +
+            "        \"entrance_img\": \"http://i2.sleeve.7yue.pro/m10.png\",\n" +
+            "        \"extend\": null,\n" +
+            "        \"internal_top_img\": \"http://i2.sleeve.7yue.pro/m12.png\",\n" +
+            "        \"title_img\": \"\",\n" +
+            "        \"tpl_name\": \"irelia\",\n" +
+            "        \"online\": true\n" +
             "    }\n" +
             "]";
 
-    public static String getThemeByNamewithSpu="\"id\":6,\n" +
+    public static String getThemeByNamewithSpu="{\"id\":6,\n" +
             "    \"title\":\"时尚穿搭\",\n" +
             "    \"description\":\"帅点才有女朋友\",\n" +
             "    \"name\":\"t-4\",\n" +
@@ -49,7 +85,188 @@ public class ThemeTestData {
             "            \"tags\":\"包邮$热门\",\n" +
             "            \"is_test\":true,\n" +
             "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
+            "        },\n" +
+            "\t\t\t\t {\n" +
+            "            \"id\":2,\n" +
+            "            \"title\":\"Sleeve羊绒毛衣\",\n" +
+            "            \"subtitle\":\"Sleeve风袖说当季经典款\",\n" +
+            "            \"category_id\":17,\n" +
+            "            \"root_category_id\":3,\n" +
+            "            \"price\":\"77.00\",\n" +
+            "            \"img\":\"\",\n" +
+            "            \"for_theme_img\":\"\",\n" +
+            "            \"description\":null,\n" +
+            "            \"discount_price\":\"62.00\",\n" +
+            "            \"tags\":\"包邮$热门\",\n" +
+            "            \"is_test\":true,\n" +
+            "            \"online\":true,\n" +
             "        }\n" +
+            "\n" +
             "    ]\n" +
             "  }";
 
