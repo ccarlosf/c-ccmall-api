58e11fe2803d3f34c7c2029bd6d314f88eac9da2
ccarlos <2238513100@qq.com>
2019-11-21 23:06:40 +0800

    Spu商品Api模拟数据接口

--- src/main/java/com/ccarlos/cccmall/controller/SpuController.java
+++ src/main/java/com/ccarlos/cccmall/controller/SpuController.java
@@ -1,11 +1,9 @@
 package com.ccarlos.cccmall.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.ccarlos.cccmall.data.BannerTestData;
 import com.ccarlos.cccmall.data.SpuTestData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
