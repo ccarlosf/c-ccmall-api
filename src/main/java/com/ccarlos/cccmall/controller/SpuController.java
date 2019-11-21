0a8d156ce3a892375f088ed3dfcade10e113a790
ccarlos <2238513100@qq.com>
2019-11-21 23:06:14 +0800

    Spu商品Api模拟数据接口

---
+++ src/main/java/com/ccarlos/cccmall/controller/SpuController.java
@@ -0,0 +1,30 @@
+package com.ccarlos.cccmall.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.ccarlos.cccmall.data.BannerTestData;
+import com.ccarlos.cccmall.data.SpuTestData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * @description: Spu商品控制器
+ * @author: Created by ccarlos
+ * @date: 2019/11/21 22:52
+ */
+@Slf4j
+@RestController
+public class SpuController {
+
+    @GetMapping("/latest")
+    public String getLatestSupList(HttpServletRequest request) {
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        log.info("获取最新Spu商品列表,请求参数parameterMap:{}", JSON.toJSONString(parameterMap));
+        log.info("获取最新Spu商品列表,,请求返回数据:{}", SpuTestData.latestSpuList);
+        return SpuTestData.latestSpuList;
+    }
+}
---
+++ src/main/java/com/ccarlos/cccmall/data/SpuTestData.java
@@ -0,0 +1,256 @@
+package com.ccarlos.cccmall.data;
+
+/**
+ * @description: Spu商品Api测试数据
+ * @author: Created by ccarlos
+ * @date: 2019/11/21 22:58
+ */
+public class SpuTestData {
+
+    public static String latestSpuList="{\n" +
+            "    \"total\": 17,\n" +
+            "    \"count\": 17,\n" +
+            "    \"page\": 0,\n" +
+            "    \"total_page\": 1,\n" +
+            "    \"items\": [\n" +
+            "        {\n" +
+            "            \"id\": 23,\n" +
+            "            \"title\": \"双色牛仔裤\",\n" +
+            "            \"subtitle\": \"秋冬新款，做一个Cool Boy\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/n11.png\",\n" +
+            "            \"for_theme_img\": \"http://i1.sleeve.7yue.pro/assets/702f2ce9-5729-4aa4-aeb3-921513327747.png\",\n" +
+            "            \"price\": \"1399\",\n" +
+            "            \"discount_price\": null,\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 10,\n" +
+            "            \"title\": \"碳素墨水\",\n" +
+            "            \"subtitle\": \"虽然我们早已不再使用钢笔书写，但钢笔在纸上划过的笔触永远是键盘无法替代的。一只钢笔+一瓶墨水在一个安静的午后，写写内心的故事。\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/m24.png\",\n" +
+            "            \"for_theme_img\": \"\",\n" +
+            "            \"price\": \"80.00\",\n" +
+            "            \"discount_price\": \"69.00\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"\",\n" +
+            "            \"sketch_spec_id\": null,\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 3,\n" +
+            "            \"title\": \"抹茶小橡皮\",\n" +
+            "            \"subtitle\": \"小作文写错了，用它擦一擦\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/m17.png\",\n" +
+            "            \"for_theme_img\": \"https://gitee.com/lrelia7/sleeve-static/raw/master/theme/spu2.png\",\n" +
+            "            \"price\": \"29.99\",\n" +
+            "            \"discount_price\": \"17.00\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"一飞推荐\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 15,\n" +
+            "            \"title\": \"多彩别针、回形针\",\n" +
+            "            \"subtitle\": \"每盒70个，可爱多彩\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/m26.png\",\n" +
+            "            \"for_theme_img\": null,\n" +
+            "            \"price\": \"24\",\n" +
+            "            \"discount_price\": \"19.9\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"三色可选\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 14,\n" +
+            "            \"title\": \"Ins 复古小夹子（Mini)\",\n" +
+            "            \"subtitle\": \"静静的，享受时光的流逝\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/m23.png\",\n" +
+            "            \"for_theme_img\": null,\n" +
+            "            \"price\": \"19.9\",\n" +
+            "            \"discount_price\": null,\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"三色可选\",\n" +
+            "            \"sketch_spec_id\": null,\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 8,\n" +
+            "            \"title\": \"Ins复古碎花NoteBook\",\n" +
+            "            \"subtitle\": \"林白默默的掏出小本本，将她说的话一次不漏的记了下来。\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/m19.png\",\n" +
+            "            \"for_theme_img\": \"http://i1.sleeve.7yue.pro/assets/b6442702-4810-46cb-bb0b-f4602d38e4ff.png\",\n" +
+            "            \"price\": \"29.99\",\n" +
+            "            \"discount_price\": \"27.8\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"林白推荐\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 28,\n" +
+            "            \"title\": \"Ins复古金色落地灯\",\n" +
+            "            \"subtitle\": \"Instagram复古台灯，就在此刻回到过去，找寻逝去的记忆\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/a9.png\",\n" +
+            "            \"for_theme_img\": \"\",\n" +
+            "            \"price\": \"999\",\n" +
+            "            \"discount_price\": null,\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"Ins$复古流行\",\n" +
+            "            \"sketch_spec_id\": \"8\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 26,\n" +
+            "            \"title\": \"SemiConer柔质沙发\",\n" +
+            "            \"subtitle\": \"窝在沙发上，一杯红酒配电影\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/3.png\",\n" +
+            "            \"for_theme_img\": \"\",\n" +
+            "            \"price\": \"4799\",\n" +
+            "            \"discount_price\": \"4200\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 25,\n" +
+            "            \"title\": \"复古双色沙发\",\n" +
+            "            \"subtitle\": \"双色可选，经典青黄两色\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/h3.png\",\n" +
+            "            \"for_theme_img\": \"\",\n" +
+            "            \"price\": \"3999\",\n" +
+            "            \"discount_price\": null,\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"复刻经典$双色可选\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 24,\n" +
+            "            \"title\": \"秋冬新款驼色大衣\",\n" +
+            "            \"subtitle\": \"2020新款，暖暖过秋冬\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/n3.png\",\n" +
+            "            \"for_theme_img\": \"http://i1.sleeve.7yue.pro/assets/b8e510a1-8340-43c2-a4b0-0e56a40256f9.png\",\n" +
+            "            \"price\": \"2999\",\n" +
+            "            \"discount_price\": \"2699\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"经典单色\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 27,\n" +
+            "            \"title\": \"Mier双色靠椅\",\n" +
+            "            \"subtitle\": \"安静的午后，一杯清茶，追忆似水年华。看清风浮动，看落日余晖\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/h1.png\",\n" +
+            "            \"for_theme_img\": \"http://i1.sleeve.7yue.pro/assets/f6c9fce8-626f-44c0-a709-3f6ef9f3fbef.png\",\n" +
+            "            \"price\": \"1299\",\n" +
+            "            \"discount_price\": null,\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 12,\n" +
+            "            \"title\": \"林间有风测试商品\",\n" +
+            "            \"subtitle\": \"测试商品，可购买体验完整支付和订单流程\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/x1.png\",\n" +
+            "            \"for_theme_img\": \"\",\n" +
+            "            \"price\": \"0.2\",\n" +
+            "            \"discount_price\": null,\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"测试数据$可支付\",\n" +
+            "            \"sketch_spec_id\": null,\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 11,\n" +
+            "            \"title\": \"飞行员墨镜\",\n" +
+            "            \"subtitle\": \"戴起来像小李子\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/n2.png\",\n" +
+            "            \"for_theme_img\": \"\",\n" +
+            "            \"price\": \"77.00\",\n" +
+            "            \"discount_price\": null,\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": null,\n" +
+            "            \"sketch_spec_id\": null,\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 6,\n" +
+            "            \"title\": \"Sleeve风袖说牛仔系列\",\n" +
+            "            \"subtitle\": \"Sleeve风袖说当季经典款\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/n14.png\",\n" +
+            "            \"for_theme_img\": \"\",\n" +
+            "            \"price\": \"1799\",\n" +
+            "            \"discount_price\": \"\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"包邮$热门\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 2,\n" +
+            "            \"title\": \"林间有风自营针织衫\",\n" +
+            "            \"subtitle\": \"秋日冬款，浪漫满屋\",\n" +
+            "            \"img\": \"http://i1.sleeve.7yue.pro/assets/ecf8d824-19d4-4db2-a5da-872ab014fecd.png\",\n" +
+            "            \"for_theme_img\": \"https://gitee.com/lrelia7/sleeve-static/raw/master/theme/spu1.png\",\n" +
+            "            \"price\": \"77.00\",\n" +
+            "            \"discount_price\": \"62.00\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"秋日冬款$浪漫满屋\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 4,\n" +
+            "            \"title\": \"印花桌布\",\n" +
+            "            \"subtitle\": \"生活需要仪式感，吃饭也一样。桌旗+桌布给你绚烂的生命色彩\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/n10.png\",\n" +
+            "            \"for_theme_img\": \"\",\n" +
+            "            \"price\": \"119.00\",\n" +
+            "            \"discount_price\": \"97.00\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"风袖臻选\",\n" +
+            "            \"sketch_spec_id\": \"5\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 1,\n" +
+            "            \"title\": \"青锋大碗\",\n" +
+            "            \"subtitle\": \"大碗主要用来盛宽面，凡凡倾情推荐\",\n" +
+            "            \"img\": \"http://i2.sleeve.7yue.pro/n9.png\",\n" +
+            "            \"for_theme_img\": \"\",\n" +
+            "            \"price\": \"12.99\",\n" +
+            "            \"discount_price\": \"11.11\",\n" +
+            "            \"description\": null,\n" +
+            "            \"tags\": \"林白推荐\",\n" +
+            "            \"sketch_spec_id\": \"1\",\n" +
+            "            \"max_purchase_quantity\": null,\n" +
+            "            \"min_purchase_quantity\": null\n" +
+            "        }\n" +
+            "    ]\n" +
+            "}";
+}
